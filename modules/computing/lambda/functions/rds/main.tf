3bb7661a72dae28a64fe063f756c0f34158807f8
Cloud User <centos@ip-172-31-83-194.ec2.internal>
2021-11-11 19:57:07 +0000

    ss

--- modules/computing/lambda/functions/rds/main.tf
+++ modules/computing/lambda/functions/rds/main.tf
@@ -1,4 +1,4 @@
-data "archive_file" "lambda_zip" {
+data "archive_file" "create_lambda_zip" {
     type          = "zip"
     source_file   = "${path.module}/create_rds_mysql_db_snapshot.py"
     output_path   = "${path.module}/create_rds_mysql_db_snapshot.zip"
@@ -15,7 +15,7 @@ resource "aws_s3_bucket_object" "this" {
  source = "${path.module}/create_rds_mysql_db_snapshot.zip"
 }
 
-resource "aws_lambda_function" "test_lambda" {
+resource "aws_lambda_function" "create_lambda" {
   s3_bucket = aws_s3_bucket.mybucket.id
   s3_key = "create_rds_mysql_db_snapshot.zip"
   function_name    = "create_rds_mysql_db_snapshot"
@@ -57,4 +57,35 @@ resource "aws_iam_policy_attachment" "iam_policy_attachment_for_lambda" {
   name = "iam_policy_attachment_for_lambda"
   roles = ["${aws_iam_role.iam_role_for_lambda.name}"]
   policy_arn = "${aws_iam_policy.iam_policy_for_lambda.arn}"
+}
+
+
+
+
+
+
+data "archive_file" "transfer_lambda_zip" {
+    type          = "zip"
+    source_file   = "${path.module}/transfer_rds_mysql_db_snapshot.py"
+    output_path   = "${path.module}/transfer_rds_mysql_db_snapshot.zip"
+}
+
+resource "aws_s3_bucket_object" "this_zip" {
+ bucket = aws_s3_bucket.mybucket.id
+ key    = "transfer_rds_mysql_db_snapshot.zip"
+ source = "${path.module}/transfer_rds_mysql_db_snapshot.zip"
+}
+
+resource "aws_lambda_function" "transfer_lambda" {
+  s3_bucket = aws_s3_bucket.mybucket.id
+  s3_key = "transfer_rds_mysql_db_snapshot.zip"
+  function_name    = "transfer_rds_mysql_db_snapshot"
+  role             = "${aws_iam_role.iam_role_for_lambda.arn}"
+  handler          = "transfer_rds_mysql_db_snapshot.lambda_handler"
+  timeout = "300"
+  vpc_config {
+    subnet_ids         = ["${var.privatesubnet}"]
+    security_group_ids = ["${var.websg}"]
+  }
+  runtime          = "python3.7"
 }
